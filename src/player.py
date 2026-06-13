2c42507612ce916975f567e124e42dcac7364ac7
tanmai-dev <tanmayb301@gmail.com>
2026-06-14 02:52:47 +0530

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,15 +1,39 @@
-class goti:
-    def __init__(self, color, number):
-        self.color = color
-        self.number = number
-        self.position = "base"
+import random
 
 class Player:
     def __init__(self, color):
-        self.piece1 = goti(color, 1)
-        self.piece2 = goti(color, 2)
-        self.piece3 = goti(color, 3)
-        self.piece4 = goti(color, 4)
-    
+        self.active = False
+        self.color = color
+        self.current_piece = None
+        self.in_pieces = 0
+
     def move_piece(self, dice, game_state):
-            
+
+        legal_moves = []
+
+        for i in range(1, 5):
+            piece = self.color + str(i)
+
+            if piece not in game_state.prog:
+                continue
+
+            prog = game_state.prog[piece]
+
+            # piece in home
+            if prog == 0:
+                if dice == 6:
+                    legal_moves.append(piece)
+
+            # piece already active
+            else:
+                if prog + dice <= 57:
+                    legal_moves.append(piece)
+
+        if legal_moves:
+            return random.choice(legal_moves)
+
+        # no legal move exists
+        for i in range(1, 5):
+            piece = self.color + str(i)
+            if piece in game_state.prog:
+                return piece
