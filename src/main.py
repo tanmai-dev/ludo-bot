59f318eb94c2f2a6fda73dca02d66258d8aff24f
Tanmay Bansal <tanmayb301@gmail.com>
2026-06-14 03:50:36 +0530

    Final Draft Game_state

--- src/main.py
+++ src/main.py
@@ -2,19 +2,25 @@ from game_state_alt import GameState
 from player import Player
 from player_alt import EvilPlayer
 
-p1 = Player("R")
-p2 = Player("G")
-p3 = Player("Y")
-p4 = EvilPlayer("B")
+score = {"R":0, "G":0, "Y":0, "B":0}
+for i in range(1000):
+    
+    p1 = Player("R")
+    p2 = Player("G")
+    p3 = Player("Y")
+    p4 = Player("B")
+    game = GameState(p1, p2, p3, p4)
+    while True:
+        result = game.update_board()
 
-game = GameState(p1, p2, p3, p4)
+        if result == "terminated":
+            print("Game terminated.")
+            break
 
-while True:
-    result = game.update_board()
+        if result == "over":
+            for j in range(3):
+                print(game.winners)
+                score[game.winners[j]] += 3-j
+            break
 
-    if result == "terminated":
-        print("Game terminated.")
-        break
-
-    if result == "over":
-        break
+print(score)
