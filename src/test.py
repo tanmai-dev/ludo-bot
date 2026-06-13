82a7565fe79b677d6c7cac8ca5dc1469acd7ddd0
Tanmay Bansal <tanmayb301@gmail.com>
2026-06-14 00:37:57 +0530

    capture

--- src/test.py
+++ src/test.py
@@ -1,12 +1,12 @@
-board = dict()
-for i in ["R", "G", "B", "Y"]:
-    for j in range(1, 13):
-        board[i+str(j)] = 0
-    for j in range(1, 6):
-        board[i+str(j)+"L"] = 0
-    for j in range(1, 5):
-        board[i+str(j)+"B"] = 0
-    board[i+"W"] = 0
-print(board)
+def capture(self, piece):
 
-print('hello world')
+    if self.board[piece] > 52:
+        self.board[piece] -= 52
+    
+    if self.board[piece] not in [1, 9, 14, 22, 27, 35, 40, 48]:
+        for i in self.board:
+            if self.board[i] == self.board[piece] and i != piece and self.board[i] != 'L' and i[0]!= piece[0]:
+                self.board[i] = 0
+                self.prog[i] = 0
+            elif self.board[i] == self.board[piece] and i != piece and self.board[i] != 'L' and i[0] == piece[0]:
+                return False
