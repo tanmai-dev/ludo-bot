32e6fadb414c1c10be80dd4eff852f95c8861997
saarthakwadhwaa <saarthakwadhwaa@gmail.com>
2026-06-14 22:51:42 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from game_state import GameState
 from random_bot import Player
+from SW_LudoBot1 import SW_LudoBot1
 
 score = {"R":0, "G":0, "Y":0, "B":0}
 for i in range(1000):
     
     p1 = Player("R")
     p2 = Player("G")
-    p3 = Player("Y")
-    p4 = Player("B")
+    p3 = SW_LudoBot1("Y")
+    p4 = SW_LudoBot1("B")
     game = GameState(p1, p2, p3, p4)
     while True:
         result = game.update_board()
